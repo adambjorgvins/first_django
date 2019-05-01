2415787cb9ae439a5dad5663f0051b3ab7edca33
adambjorgvins <adambjorgvinsson@gmail.comlscd ..hellocd max_nums.py git config --global user.name adambjorgvinsgit config --global user.email adambjorgvinsson@gmail.com>
2019-05-01 23:04:16 +0000

    no more tests

--- pages/urls.py
+++ pages/urls.py
@@ -2,7 +2,7 @@
 from django.urls import path
 
 from .views import HomePageView, AboutPageView
-#test git
+
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
     path('about/', AboutPageView.as_view(), name='about'),
